fd2ed0602106538bf9dd9fca96fdfb92fb944b94
mamakow <git@martin-mandelkow.de>
2026-04-08 15:24:33 +0200

    Migrate to version catalogs and upgrade Gradle, AGP, and Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     //alias(libs.plugins.android.application)
-    id("com.android.application") version "8.13.2"
-    id("org.jetbrains.kotlin.android") version "2.0.0"
-
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
