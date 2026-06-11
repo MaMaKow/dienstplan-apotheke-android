a74fa12972a038181b08396d3b5caeab1acdc45f
mamakow <git@martin-mandelkow.de>
2026-06-11 12:38:09 +0200

    Update build configuration and dependencies - Bump targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "de.mamakow.dienstplanapotheke"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
